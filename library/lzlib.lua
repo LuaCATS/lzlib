fd94ede329cda1f7710d2b0b9e545bc3981d2548
Josef Friedrich <josef@friedrich.rocks>
2023-03-06 16:57:49 +0100

    Add definition

---
+++ library/lzlib.lua
@@ -0,0 +1,29 @@
+---@meta
+
+---
+---`lzlib`, by Tiago Dionizio, http://luaforge.net/projects/lzlib/.
+---https://luarocks.org/modules/hisham/lzlib
+---https://github.com/LuaDist/lzlib
+zlib = {}
+
+
+function zlib.adler32()
+end
+
+function zlib.compress() end
+
+
+function zlib.compressobj() end
+
+
+function zlib.crc32() end
+
+
+function zlib.decompress() end
+
+
+function zlib.decompressobj() end
+
+function zlib.version() end
+
+return zlib

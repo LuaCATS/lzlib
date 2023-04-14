b1ec0d75bf5fcb89bbddbc107354e50df2ec9b17
Josef Friedrich <josef@friedrich.rocks>
2023-04-14 18:27:22 +0200

    Add contribution links

--- library/lzlib.lua
+++ library/lzlib.lua
@@ -5,12 +5,16 @@
 ---`lzlib`, by Tiago Dionizio, http://luaforge.net/projects/lzlib/.
 ---https://luarocks.org/modules/hisham/lzlib
 ---https://github.com/LuaDist/lzlib
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 local zlib = {}
 
 ---
 ---returns zlib version
 ---
 ---@return string
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function zlib.version() end
 
 ---
@@ -23,6 +27,8 @@ function zlib.version() end
 ---@param buffer? string
 ---
 ---@return string buffer
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function zlib.adler32(adler32, buffer) end
 
 ---
@@ -32,6 +38,8 @@ function zlib.adler32(adler32, buffer) end
 ---@param buffer? string
 ---
 ---@return string buffer
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function zlib.crc32(crc32, buffer) end
 
 ---
@@ -45,6 +53,8 @@ function zlib.crc32(crc32, buffer) end
 ---@param strategy? integer
 ---
 ---@return string buffer
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function zlib.compress(buffer, level, method, windowBits, memLevel, strategy) end
 
 ---
@@ -54,13 +64,25 @@ function zlib.compress(buffer, level, method, windowBits, memLevel, strategy) en
 ---@param windowBits? integer
 ---
 ---@return string
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function zlib.decompress(buffer, windowBits) end
 
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 ---@class Sink
 local Sink = {}
 
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function Sink:write() end
+
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function Sink:close() end
+
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function Sink:flush() end
 
 ---
@@ -75,12 +97,19 @@ function Sink:flush() end
 ---@param dictionary? string - default `""`
 ---
 ---@return Stream
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function zlib.deflate(sink, level, method, windowBits, memLevel, strategy, dictionary) end
 
 ---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 ---@class InflateSink
 local InflateSink = {}
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function InflateSink:read() end
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function InflateSink:close() end
 
 ---
@@ -90,9 +119,12 @@ function InflateSink:close() end
 ---@param windowBits? integer -  default `15`
 ---@param dictionary? string - default `""`
 ---@return Stream
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function zlib.inflate(source, windowBits, dictionary) end
 
 ---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 ---@class Stream
 local Stream = {}
 
@@ -100,6 +132,8 @@ local Stream = {}
 ---Write each parameter into the stream.
 ---
 ---@param ... any
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function Stream:write(...) end
 
 ---
@@ -112,21 +146,29 @@ function Stream:write(...) end
 ---    'a' - reads the remaining bytes
 ---    'l' - reads a line
 ---@param ... integer|'a'|'l'
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function Stream:read(...) end
 
 ---
 ---Returns an iterator that returns a new line each time
 ---it is called.
 ---@return function
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function Stream:lines() end
 
 ---
 ---Flush output for deflate streams.
 ---@param opts 'sync'|'full'|'finish'
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function Stream:flush(opts) end
 
 ---
 ---Close the stream.
+---
+---😱 [Types](https://github.com/LuaCATS/lzlib/blob/main/library/lzlib.lua) incomplete or incorrect? 🙏 [Please contribute!](https://github.com/LuaCATS/lzlib/pulls)
 function Stream:close() end
 
 return zlib
